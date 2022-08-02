19c8063795826dac4af3b2641d0b9ba6af0e75a9
Zaid Marrie <zaidmarrie27@gmail.com>
2022-08-02 14:14:29 +0200

    build rotating navigation mini-project

--- 03-rotating-navigation/script.js
+++ 03-rotating-navigation/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const openButton = document.getElementById("openBtn");
+const closeButton = document.getElementById("closeBtn");
+const container = document.querySelector(".container");
+
+openButton.addEventListener("click", () => container.classList.add("show-nav"));
+
+closeButton.addEventListener("click", () =>
+	container.classList.remove("show-nav")
+);
