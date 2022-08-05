b864861af6e256f2b8631470bb3f08c12dbca868
Zaid Marrie <zaidmarrie27@gmail.com>
2022-08-05 11:51:23 +0200

    build mini-project (Scroll Animation)

--- 06-scroll-animation/script.js
+++ 06-scroll-animation/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+	const triggerPoint = window.innerHeight;
+
+	boxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < (triggerPoint / 2) * 1.7) {
+			box.classList.add("show");
+		} else {
+			box.classList.remove("show");
+		}
+	});
+}
+
+checkBoxes();
