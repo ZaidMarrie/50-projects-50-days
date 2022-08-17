25b2cb45f11631b7398fb05c89efdace9c5abd06
Zaid Marrie <zaidmarrie27@gmail.com>
2022-08-17 12:31:27 +0200

    build mini-project(Split Landing Page)

--- 07-split-landing-page/script.js
+++ 07-split-landing-page/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+left.addEventListener("mouseenter", (event) => {
+	left.classList.add("active");
+});
+
+left.addEventListener("mouseleave", (event) => {
+	left.classList.remove("active");
+});
+
+right.addEventListener("mouseenter", (event) => {
+	right.classList.add("active");
+});
+
+right.addEventListener("mouseleave", (event) => {
+	right.classList.remove("active");
+});
